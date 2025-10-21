c2629515e3c26477fd74c6f886632a72c5ba7276
y-nagase <y-nagase@nagase-system.com>
2025-10-21 20:59:24 +0900

    fix: add a disclaim area to confirm page

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -51,7 +51,9 @@ export async function updateSession(request: NextRequest) {
         !(request.nextUrl.pathname === '/') &&
         !request.nextUrl.pathname.startsWith('/login') &&
         !request.nextUrl.pathname.startsWith('/auth') &&
-        !request.nextUrl.pathname.startsWith('/error')
+        !request.nextUrl.pathname.startsWith('/error') &&
+        !request.nextUrl.pathname.startsWith('/terms') &&
+        !request.nextUrl.pathname.startsWith('/privacy')
     ) {
         // no user, potentially respond by redirecting the user to the login page
         const url = request.nextUrl.clone();
@@ -66,7 +68,9 @@ export async function updateSession(request: NextRequest) {
             request.nextUrl.pathname.startsWith('/login') ||
             request.nextUrl.pathname.startsWith('/auth') ||
             request.nextUrl.pathname.startsWith('/confirm') ||
-            request.nextUrl.pathname.startsWith('/error');
+            request.nextUrl.pathname.startsWith('/error') ||
+            request.nextUrl.pathname.startsWith('/terms') ||
+            request.nextUrl.pathname.startsWith('/privacy');
 
         // Only check terms acceptance for protected paths
         if (!isPublicPath) {
